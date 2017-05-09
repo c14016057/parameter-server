2f6c34bdbbe4e83b18a7584620ba983e8e9fb4b1
Yuan <c14016057@gmail.com>
2017-05-09 20:04:23 +0800

    add server.cpp client.cpp

--- GRPC/client.cpp
+++ GRPC/client.cpp
@@ -5,9 +5,8 @@
 #include <grpc++/grpc++.h>
 #include "keyvector.grpc.pb.h"
 
-#define NUM_KEY 5000
-#define NUM_VALUE_PERKEY 1000
-
+#define NUM_KEY 500
+#define NUM_VALUE 100
 #define THREAD_NUM 1000
 
 using grpc::Channel;
@@ -27,97 +26,104 @@ class paramClient {
 		paramClient(std::shared_ptr<Channel> channel) : stub_(pushPullRequest::NewStub(channel)) {}
 
 
-	void pull(int* key, int numKey){
-		ClientContext context;
-		keyMessage keyMsg;
-		vectorMessage response;
+		void pull(int* key, int numKey) {
+			ClientContext context;
+			keyMessage keyMsg;
+			vectorMessage resValue;
 
-		for(int i = 0 ; i < numKey ; ++i)
-			keyMsg.add_key( key[i] );
-		
-		
+			for(int i = 0 ; i < numKey ; ++i)
+				keyMsg.add_key( key[i] );
 
-		Status status = stub_->pull(&context, keyMsg, &response);	
-		if (status.ok()) 
-			std::cout << "ok, Pulled. avg = " << response.val(0) << std::endl;
-		else{ 
-			std::cout << status.error_code() << ": " << status.error_message() << std::endl;
-			return ;
+
+
+			Status status = stub_->pull(&context, keyMsg, &resValue);	
+			if (status.ok()) { 
+				for (int i = 0; i < NUM_KEY; i++)
+					for (int j = 0; j <NUM_VALUE; j++)
+						if (resValue.val(i*NUM_VALUE+j) !=1 ) {
+							std::cout << "error avg" << std::endl;
+							return;
+						}
+				cout << "ac avg" << std::endl;
+			}
+			else{ 
+				std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+				return ;
+			}
+
+			//		cout << response.val(0) << endl;
 		}
 
-//		cout << response.val(0) << endl;
 
-		
-	}
+		void push(int* keys, double** vals, int numKey, int numVal, int iter) {
+			ClientContext context;
+			keyVectorMessage kvMsg;
+			Empty response;
 
 
-	void push(int* keys, double** vals, int numKey, int numVal, int iter){
-		ClientContext context;
-                keyVectorMessage kvMsg;
-                Empty response;
+			for(int i = 0 ; i < numKey ; ++i) {
+				kvMsg.add_key( keys[i] );
+				for(int j = 0 ; j < numVal ; ++j)
+					kvMsg.add_val( vals[i][j] );
+			}
+			kvMsg.set_iter(iter);
 
-		
-		for(int i = 0 ; i < numKey ; ++i){
-			kvMsg.add_key( keys[i] );
-			for(int j = 0 ; j < numVal ; ++j)
-				kvMsg.add_val( vals[i][j] );
+
+			Status status = stub_->push(&context, kvMsg, &response);
+			if (status.ok()) 
+				cout << "ok, Pushed." << endl;
+			else
+				cout << status.error_code() << ": " << status.error_message() << endl;
+
 		}
-		kvMsg.set_iter(iter);
 
-
-                Status status = stub_->push(&context, kvMsg, &response);
-                if (status.ok()) 
-                        cout << "ok, Pushed." << endl;
-                else
-                        cout << status.error_code() << ": " << status.error_message() << endl;
-		
-	}
-	
 	private:
 		std::unique_ptr<pushPullRequest::Stub> stub_;
 };
 
 
-double dRandGen(double dMin, double dMax){
+double dRandGen(double dMin, double dMax) {
 	double dRand = (double)rand() / RAND_MAX;
 	return dMin + dRand * (dMax - dMin);
 
 }
 
 int main(int argc, char** argv) {
-	paramClient client(grpc::CreateChannel("140.112.30.241:50051", grpc::InsecureChannelCredentials()));
+	//paramClient client(grpc::CreateChannel("127.0.0.1:50051", grpc::InsecureChannelCredentials()));
 
-
+	grpc::ChannelArguments channelArgs;
+    channelArgs.SetInt("GRPC_ARG_MAX_MESSAGE_LENGTH", 100 * 1024 * 1024);
+	paramClient client(grpc::CreateCustomChannel("127.0.0.1:50051", grpc::InsecureChannelCredentials(), channelArgs));
 	int* keys = new int[NUM_KEY];
 	for(int i = 0 ; i < NUM_KEY ; ++i)
-                keys[i] = i+1;
+		keys[i] = i;
 
 	double** vals = new double *[NUM_KEY];
 	for(int i = 0 ; i < NUM_KEY ; ++i)
-	    vals[i] = new double[NUM_VALUE_PERKEY];
+		vals[i] = new double[NUM_VALUE];
 
-	
-	for(int i = 0 ; i < 10 ; ++i){      // ML Iteration.
+
+	for(int i = 0 ; i < 10000 ; ++i) {      // ML Iteration.
 
 		for(int j = 0 ; j < NUM_KEY ; ++j)
-			for(int k = 0 ; k < NUM_VALUE_PERKEY ; ++k)
-				vals[j][k] = dRandGen(0, 1);
-	
-		client.push(keys, (double **)vals, NUM_KEY, NUM_VALUE_PERKEY, i);
-	
-		
+			for(int k = 0 ; k < NUM_VALUE ; ++k)
+				vals[j][k] = 1;
+
+		client.push(keys, (double **)vals, NUM_KEY, NUM_VALUE, i);
+
+
 		//	DO ML COMPUTE.
-		
+
 
 		client.pull(keys, NUM_KEY);
 	}
-	
-	
+
+
 	for(int i = 0 ; i < NUM_KEY ; ++i)
-            delete [] vals[i];
+		delete [] vals[i];
 	delete [] vals;
 	delete [] keys;
-	
+
 
 	return 0;
 }
--- GRPC/server.cpp
+++ GRPC/server.cpp
@@ -20,109 +20,115 @@ using keyvector::keyVectorMessage;
 using std::cout;
 using std::endl;
 
-#define NUM_CLIENT 2
-
+#define NUM_CLIENT 25
+//#define MAX_CLIENT 16
+#define MAX_KEY 500
+#define MAX_VALUE 100
 int readyCount;
-int pullCount;
-bool isAggregated;
-double avg;
-std::mutex mutex_;
+int pullCount = 0, pushCount = 0;
+bool isAggregated, canPush = true, canPull = false;
+//double valueTable[MAX_CLIENT][MAX_KEY][MAX_VALUE] = {};
+double avgValue[MAX_KEY][MAX_VALUE]= {};
+std::mutex pull_mutex, push_mutex;
 
-double aggregate(int *keys) {
-        // Do Sync.
-
-        while(pullCount != 0)
-                ;
-
-        mutex_.lock();
-        ++readyCount;
-        mutex_.unlock();
-
-        while(readyCount != NUM_CLIENT)
-                ;
-
-        mutex_.lock();
-        if(isAggregated == false) {
-                // do average
-
-                avg = 0.87;
-                isAggregated = true;
-                ++pullCount;
-                mutex_.unlock();
-        }
-        else {
-                // read average
-                avg = 0.78;
-
-                if( ++pullCount == NUM_CLIENT ) {
-                        isAggregated = false;
-                        pullCount = 0;
-                        readyCount = 0;
-                }
-                mutex_.unlock();
-        }
-
-        return avg;
+double * aggregate(int *keys, int numkey) {
+	if(isAggregated == false) {
+		// do average
+		for (int i = 0; i < MAX_KEY; i++)
+			for (int j = 0; j < MAX_VALUE; j++)
+				avgValue[i][j] /= (double)NUM_CLIENT;
+		isAggregated = true;
+	}
+	return avgValue[0];
 }
 
 class ParamServerServiceImpl final : public pushPullRequest::Service {
 
-  Status push(ServerContext* context, const keyVectorMessage* request, Empty* response) override {
+	Status push(ServerContext* context, const keyVectorMessage* vector, Empty* response) override {
+
+		//Wait for all pull done
+		while(!canPush);
+		cout << "Pushed. Iteration:" << vector->iter() << endl;
 
 
-        cout << "Pushed. Iteration:" << request->iter() << endl;
+		//Push
+		push_mutex.lock();
+		for (int i = 0 ; i < MAX_KEY ; ++i) {
+			for (int j = 0; j < MAX_VALUE; j++) {
+				avgValue[i][j] += vector->val(i*MAX_VALUE + j);
+			}
+		}
 
-        /*
-        for(int i = 1 ; i < 100 ; ++i){
-                cout << request->key(i) << ":" << request->val(i) << endl;
-        }*/
-
-
-        return Status::OK;
-  }
-
-  Status pull(ServerContext* context, const keyMessage* request, vectorMessage* response) override {
-
-        int numkey = request->key_size();
-        int *keys = new int[numkey];
-
-        cout << "Pulled." << endl;
-
-
-        for(int i = 0 ; i < numkey ; ++i){
-                keys[i] = request->key(i);
-        }
-
-        double avg = aggregate(keys);
-
-        response->add_val(avg);
+		if (++pushCount == NUM_CLIENT) {
+			//ALL CLIENT IS PUSHED
+			pushCount = 0;
+			canPull = true;
+			canPush = false;
+		}
+		push_mutex.unlock();
 
 
 
-        delete [] keys;
-        return Status::OK;
-  }
+		return Status::OK;
+	}
+
+	Status pull(ServerContext* context, const keyMessage* reqkey, vectorMessage* retValue) override {
+
+		int numkey = reqkey->key_size();
+		int *keys = new int[numkey];
+
+		//Wait for all push done.
+		while(!canPull);
+		cout << "Pulled." << endl;
+
+		for(int i = 0 ; i < numkey ; ++i){
+			keys[i] = reqkey->key(i);
+		}
+		pull_mutex.lock();
+		double *avg = aggregate(keys, numkey);
+
+		for(int i = 0; i < MAX_KEY; i++)
+			for(int j =0; j < MAX_VALUE; j++)
+				retValue->add_val( avg[i*j] );
+
+		if (++pullCount == NUM_CLIENT) {
+			//ALL CLIENT IS PULLED
+			pullCount = 0;
+			canPush = true;
+			canPull = false;
+			isAggregated = false;
+			for(int i = 0; i < MAX_KEY; i++)
+				for(int j =0; j < MAX_VALUE; j++)
+					avgValue[i][j] = 0;
+		}
+		pull_mutex.unlock();
+				
+
+
+		delete [] keys;
+		return Status::OK;
+	}
 };
 
 void RunServer() {
-        std::string server_address("0.0.0.0:50051");
-        ParamServerServiceImpl service;
+	std::string server_address("0.0.0.0:50051");
+	ParamServerServiceImpl service;
 
-        ServerBuilder builder;
-        builder.SetMaxMessageSize(100 * 1024 * 1024);
-        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-        builder.RegisterService(&service);
-        std::unique_ptr<Server> server(builder.BuildAndStart());
-        std::cout << "Server listening on " << server_address << std::endl;
+	ServerBuilder builder;
+	builder.SetMaxMessageSize(100 * 1024 * 1024);
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+	builder.RegisterService(&service);
+	std::unique_ptr<Server> server(builder.BuildAndStart());
+	std::cout << "Server listening on " << server_address << std::endl;
 
-        server->Wait();
+	server->Wait();
 }
 
 int main(int argc, char** argv) {
-        readyCount = 0;
-        pullCount = 0;
-        isAggregated = false;
-        RunServer();
+	readyCount = 0;
+	pullCount = 0;
+	isAggregated = false;
+	RunServer();
 
-        return 0;
+	return 0;
 }
